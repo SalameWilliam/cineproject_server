19a41e296774dc032752b4a0b312c2e6457e157d
Salame William <william.salame@student.uclouvain.be>
2019-06-02 16:12:50 +0200

    Fix boutton, espaces avec les liens, redirection, ...

--- routes/festivals.js
+++ routes/festivals.js
@@ -7,7 +7,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/48hfp', function(req, res, next) {
-    res.render('48hfp', { title: '48 Hour Film Project' });
+    res.render(
+        '48hfp',
+        {
+            title: '48 Hour Film Project',
+            directory: '/images/48hfp/',
+            public: {
+                imgs: {
+                    _contents:[
+                        "48hfp (1).jpg",
+                        "48hfp (2).jpg",
+                        "48hfp (3).jpg",
+                        "48hfp (4).jpg",
+                        "48hfp (5).jpg",
+                        "48hfp (6).jpg",
+                        "48hfp (7).jpg",
+                        "48hfp (8).jpg",
+                        "48hfp (9).jpg"
+                    ]
+                }
+            }
+        }
+    );
 });
 
 router.get('/partenaire', function(req, res, next) {
